c68566be314ff5454cbc3fd71daf9d46a89cd723
vetrojet <slavik.pro0101@gmail.com>
2023-09-21 14:44:43 +0300

    structure + config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanevn
+
+	// TODO: init logger: slog log/slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, 'chi render'
+
+	// TODO: run server
+}
